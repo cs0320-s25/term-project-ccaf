d0f0fd0c30f12a4593475f69d2c71a39ae3adf1b
Christie <christiecadette@gmail.com>
2025-04-21 15:59:26 -0400

    feat: added key classes and components, group work sesh (4/4)

---
+++ .github/backend/Piece.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Piece {
+
+private String id;
+private String title;
+private double price;
+private String sourceWebsite;
+private String url; 
+private String size;
+private String color;
+private String condition;
+private String imageUrl;
+private Set<String> tags;
+
+
+    public Piece(
+        String id, 
+        String title, 
+        double price, 
+        String sourceWebsite, 
+        String url, 
+        String size, 
+        String color, 
+        String condition, 
+        String imageUrl, 
+        Set<String> tags)
+         {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.sourceWebsite = sourceWebsite;
+        this.url = url;
+        this.size = size;
+        this.color = color;
+        this.condition = condition;
+        this.imageUrl = imageUrl;
+        this.tags = tags;
+    }
+
+}
