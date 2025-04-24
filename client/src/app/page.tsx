3973e817627bd442993079a19e4195bf8c6c56e2
audreychou04 <audrey_chou@brown.edu>
2025-04-24 00:28:22 -0400

    Initial clean commit after fixing corrupted repo

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">welcome to draft</h1>
+    </main>
+  )
+}
