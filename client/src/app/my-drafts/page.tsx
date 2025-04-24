1378c30aabd0925afb23c1aec7d60b476f8b5e45
Faith Matthew <faiththatisme@gmail.com>
2025-04-24 19:01:25 -0400

    feat: nav bar functional, clerk functional (if maybe misplaced)

---
+++ client/src/app/my-drafts/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "app/globals.css";
+
+
+const MyDraftsPage = () => {
+  return (
+    <div className="container px-4 py-16 max-w-5xl mx-auto">
+      <div className="text-center mb-12">
+        <h1>your saved drafts</h1>
+        <p className="body-text">
+          here's your collections of drafts!
+          <br />
+          let's get inspired!
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+export default MyDraftsPage;
