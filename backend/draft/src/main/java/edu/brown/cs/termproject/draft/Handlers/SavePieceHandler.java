79004a2d498b687d4e0ff91c75f23067889bde11
Faith Matthew <faiththatisme@gmail.com>
2025-04-25 18:38:06 -0400

    planning + working on search backend + some add functionality, pair prog w/ christie

---
+++ backend/draft/src/main/java/edu/brown/cs/termproject/draft/Handlers/SavePieceHandler.java
@@ -0,0 +1,48 @@
+//package edu.brown.cs.termproject.draft.Handlers;
+//
+//import com.google.gson.Gson;
+//import com.google.gson.JsonObject;
+//import edu.brown.cs.termproject.draft.Piece;
+//import edu.brown.cs.termproject.draft.PieceStore;
+//import edu.brown.cs.termproject.draft.util.FirebaseUtils;
+//import spark.Request;
+//import spark.Response;
+//import spark.Route;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class SavePieceHandler implements Route {
+// @Override
+// public Object handle(Request request, Response response) throws Exception {
+//   Gson gson = new Gson();
+//   Map<String, Object> responseMap = new HashMap<>();
+//
+//   try {
+//     JsonObject body = gson.fromJson(request.body(), JsonObject.class);
+//     String draftId = body.get("draftId").getAsString();
+//     String pieceId = body.get("pieceId").getAsString();
+//
+//     Piece piece = FirebaseUtils.getPieceById(pieceId);
+//
+//     if (piece == null) {
+//       response.status(400);
+//       responseMap.put("status", "error");
+//       responseMap.put("message", "Invalid pieceId");
+//       return gson.toJson(responseMap);
+//     }
+//
+//     FirebaseUtils.savePiece(draftId, piece);
+//
+//     responseMap.put("status", "success");
+//     responseMap.put("message", "Piece saved to draft in Firestore");
+//     return gson.toJson(responseMap);
+//
+//   } catch (Exception e) {
+//     response.status(500);
+//     responseMap.put("status", "error");
+//     responseMap.put("message", "Server error: " + e.getMessage());
+//     return gson.toJson(responseMap);
+//   }
+// }
+//}
