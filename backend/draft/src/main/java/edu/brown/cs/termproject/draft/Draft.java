f194d0a617e35f51220ec62d825f852020668ee0
Christie <christiecadette@gmail.com>
2025-04-25 20:32:38 -0400

    feat: regular search works but problems with integrating the eBay API. need to figure out a way to access the .env file using the APIUtilities class

--- backend/draft/src/main/java/edu/brown/cs/termproject/draft/Draft.java
+++ backend/draft/src/main/java/edu/brown/cs/termproject/draft/Draft.java
@@ -3,16 +3,16 @@ package edu.brown.cs.termproject.draft;
 import java.util.*;
 
 public class Draft {
-//  private String id;
-//  private String name;
-//  private Set<Piece> pieces = new HashSet<>();
-//
-//  public void addPiece(Piece piece) {
-//    pieces.add(piece);
-//  }
-//
-//  public Set<Piece> getPieces() {
-//    return pieces;
-//  }
+  private String id;
+  private String name;
+  private Set<Piece> pieces = new HashSet<>();
+
+  public void addPiece(Piece piece) {
+    pieces.add(piece);
+  }
+
+  public Set<Piece> getPieces() {
+    return pieces;
+  }
 }
 
